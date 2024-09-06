87aed0c95654e5f1853195cb4088bcf5462ab8b5
martivit <martina@10096.it>
2024-09-06 10:04:30 +0200

    Update 2_📊_Calculation_--_PiN.py

--- pages/2_📊_Calculation_--_PiN.py
+++ pages/2_📊_Calculation_--_PiN.py
@@ -502,13 +502,16 @@ def finalize_details():
             key='admin_target'
         )
 
-        mismatch_admin_checkbox = st.checkbox(
+        st.markdown(
             f"""
             <div style="font-size:20px; font-weight:bold;">
                 {translations['mismatch_admin_message']}
             </div>
             """, unsafe_allow_html=True
-)
+        )
+
+        # Display a simple checkbox below the message
+        mismatch_admin_checkbox = st.checkbox("Check if there is a mismatch")
         
         if st.button('Confirm Admin Level', key='confirm_admin_level'):
             if admin_target != 'No selection':
