653be99d668b520f601bf581251aed15671b1708
martivit <martina@10096.it>
2024-10-14 16:11:09 +0200

    Update calculation_for_PiN_Dimension.py

--- calculation_for_PiN_Dimension.py
+++ calculation_for_PiN_Dimension.py
@@ -1001,7 +1001,7 @@ def calculatePIN (country, edu_data, household_data, choice_data, survey_data, o
 
     secondary_end = 17
 
-    host_suggestion = ["Urban","always_lived","general_pop",'non_deplace','Host Community','host_communi', "always_lived","non_displaced_vulnerable",'host',"non_pdi","hote","menage_n_deplace","resident","lebanese","Populationnondéplacée","ocap","non_deplacee","Residents","yes","4"]
+    host_suggestion = ["Urban","always_lived","general_pop",'non_deplace','Host Community',"Host community members",'host_communi', "always_lived","non_displaced_vulnerable",'host',"non_pdi","hote","menage_n_deplace","resident","lebanese","Populationnondéplacée","ocap","non_deplacee","Residents","yes","4"]
     IDP_suggestion = ['host_family',"Rural","displaced","IDP", 'New IDPs','pdi', 'idp', 'site','idp_host' ,"menage_deplace_interne", 'Out-of-camp','no',  'pdi_fam', '2', '1' ]
     returnee_suggestion = ['displaced_previously' ,'cb_returnee','retourne','ret','Returnee HH','returnee' ,'ukrainian moldovan','Returnees','5']
     refugee_suggestion = ['refugees', 'refugee','refugie', 'prl', 'refugiee', '3']
