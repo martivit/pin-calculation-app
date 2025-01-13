86aa41743363bf8438aff5f21c25cbf680c09db8
martivit <martina@10096.it>
2025-01-13 18:00:52 +0100

    Update 3_📋_Download_--_PiN_figures_and_other_outputs.py

--- pages/3_📋_Download_--_PiN_figures_and_other_outputs.py
+++ pages/3_📋_Download_--_PiN_figures_and_other_outputs.py
@@ -239,7 +239,7 @@ if ocha_data is not None:
             repo_name = "martivit/pin-calculation-app"
             branch_name = "develop_2025"
             commit_message = f"Add PiN results for {country_label}"
-            file_path_in_repo = f"platform_PiN_output/PiN_resultspiipo_{country_label}.xlsx"
+            file_path_in_repo = f"platform_PiN_output/testPiN_resultspiipo_{country_label}.xlsx"
             github_token = st.secrets["github"]["token"]
             #github_token = "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"
 
