26be2d935dfecde1f1386d7c30e357d279989602
martivit <martina@10096.it>
2025-01-17 16:30:26 +0100

    Update 2_📊_Calculation_--_PiN.py

--- pages/2_📊_Calculation_--_PiN.py
+++ pages/2_📊_Calculation_--_PiN.py
@@ -344,7 +344,10 @@ def display_filtered_kobo(filtered_edu_kobo, string = 'show kobo'):
         # Display as a table to remove row numbers completely
         st.dataframe(filtered_edu_kobo.style.set_properties(
             **{'background-color': '#FFF1A4', 'border': '1px solid #4CAF50'}
-        ))
+        )
+        use_container_width=True  # Adjust the table to fit the container width
+
+        )
     else:
         st.warning(translations.get('no_data_found', "No matching data found for the selected criteria."))
  
