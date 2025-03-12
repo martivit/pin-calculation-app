8ddd925012c53382317c6c7b4104102ae22ddfa9
martivit <martina@10096.it>
2025-03-12 10:21:27 +0100

    Update 3_📋_Download_--_PiN_figures_and_other_outputs.py

--- pages/3_📋_Download_--_PiN_figures_and_other_outputs.py
+++ pages/3_📋_Download_--_PiN_figures_and_other_outputs.py
@@ -221,3 +221,216 @@ if ocha_data is not None:
                                                                                     admin_var, vector_cycle, start_school, status_var,
                                                                                     mismatch_admin,
                                                                                     selected_language= selected_language)
+    
+    
+
+    label_total_pin_sheet = "PiN TOTAL"
+
+    #ocha_excel = create_output(country_label,Tot_PiN_JIAF, final_overview_df, final_overview_df_OCHA, label_total_pin_sheet,  admin_var,  ocha= True, tot_severity=Tot_PiN_by_admin, selected_language=selected_language, parameters=parameters)
+    if selected_language == "French":
+        ocha_excel = create_output(
+            country_label,
+            Tot_PiN_JIAF,
+            final_overview_df,
+            final_overview_df_OCHA,
+            label_total_pin_sheet,
+            admin_var,
+            ocha=True,
+            tot_severity=Tot_PiN_by_admin,
+            selected_language=selected_language,
+            parameters=parameters_FR  
+        )
+    else:
+        ocha_excel = create_output(
+            country_label,
+            Tot_PiN_JIAF,
+            final_overview_df,
+            final_overview_df_OCHA,
+            label_total_pin_sheet,
+            admin_var,
+            ocha=True,
+            tot_severity=Tot_PiN_by_admin,
+            selected_language=selected_language,
+            parameters=parameters  
+        )
+
+    indicator_output = create_indicator_output(country_label, indicator_per_admin_status, admin_var=admin_var)
+
+    if selected_language == "English":
+        doc_output = create_snapshot_PiN(country_label, final_overview_df, final_overview_df_OCHA,final_overview_dimension_df, final_overview_dimension_df_in_need, selected_language=selected_language)
+        doc_parameter_output = generate_word_document(parameters)
+
+    if selected_language == "French":
+        doc_output = create_snapshot_PiN_FR(country_label, final_overview_df, final_overview_df_OCHA,final_overview_dimension_df, final_overview_dimension_df_in_need, selected_language=selected_language)
+        doc_parameter_output = generate_word_document_FR(parameters_FR)
+
+    zip_file_name = f"PiN_Documents_{country_label}_{datetime.now().strftime('%Y%m%d_%H%M')}.zip"
+    zip_file = create_zip_file(country_label, ocha_excel,indicator_output, doc_output, doc_parameter_output)
+
+
+    
+
+    # Create a single download button for the ZIP file
+    if st.download_button(
+        label=translations["download_all"],
+        data=zip_file,
+        file_name=zip_file_name,
+        mime="application/zip"
+    ):
+        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+
+        #if "github" in st.secrets and "token" in st.secrets["github"]:
+            #st.write("✅ GitHub token found in secrets.")
+        #else:
+            #st.error("❌ GitHub token not found in secrets. Check your Streamlit configuration.")
+
+        try:
+            repo_name = "martivit/pin-calculation-app"
+            branch_name = "develop_2025"
+
+            # File paths in the repository
+            file_path_in_repo_excel = f"platform_PiN_output/{country}/PiN_results_{country}_{timestamp}.xlsx"
+            file_path_in_repo_doc = f"platform_PiN_output/{country}/PiN_snapshot_{country}_{timestamp}.docx"
+
+            github_token = st.secrets["github"]["token"]
+
+            # Initialize success messages for both uploads
+            pr_url_excel = None
+            pr_url_doc = None
+
+            # Try uploading both files
+            try:
+                pr_url_excel = upload_to_github(
+                    file_content=ocha_excel.getvalue(),
+                    file_name=file_path_in_repo_excel,
+                    repo_name=repo_name,
+                    branch_name=branch_name,
+                    commit_message=f"Add PiN results (Excel) for {country_label}",
+                    token=github_token
+                )
+            except Exception :
+                pass
+                #st.error(f"Failed to upload Excel file to GitHub: {e}")
+
+            try:
+                pr_url_doc = upload_to_github(
+                    file_content=doc_output.getvalue(),
+                    file_name=file_path_in_repo_doc,
+                    repo_name=repo_name,
+                    branch_name=branch_name,
+                    commit_message=f"Add PiN snapshot (Word) for {country_label}",
+                    token=github_token
+                )
+            except Exception :
+                pass
+                #st.error(f"Failed to upload Word document to GitHub: {e}")
+
+            # Display success messages only if files were successfully uploaded
+            if pr_url_excel:
+                st.success(f"Excel file uploaded to GitHub successfully! [View File]({pr_url_excel})")
+            if pr_url_doc:
+                st.success(f"Word document uploaded to GitHub successfully! [View File]({pr_url_doc})")
+
+        except Exception :
+            #st.error(f"Unexpected error during GitHub upload: {e}")
+            pass
+ 
+    st.subheader(translations["hno_guidelines_subheader"])
+    st.markdown(translations["hno_guidelines_message"])
+
+
+
+
+######################################################################### no ocha data
+
+if no_ocha_data:
+    (severity_admin_status_list, dimension_admin_status_list,
+    indicator_per_admin_status,
+    country_label) = calculatePIN_NO_OCHA_2025 (country, edu_data_severity, household_data, choice_data, survey_data,mismatch_ocha_data,
+                                                                                    access_var, teacher_disruption_var, idp_disruption_var, armed_disruption_var,natural_hazard_var,
+                                                                                    barrier_var, selected_severity_4_barriers, selected_severity_5_barriers,
+                                                                                    age_var, gender_var,
+                                                                                    label, 
+                                                                                    admin_var, vector_cycle, start_school, status_var,
+                                                                                    mismatch_admin,
+                                                                                    selected_language= selected_language)
+
+    indicator_output = create_indicator_output_no_ocha(country_label, indicator_per_admin_status, admin_var=admin_var, selected_language=selected_language)
+    pin_percentage_output = create_pin_raw_output(country_label, severity_admin_status_list, admin_var=admin_var, selected_language=selected_language)
+
+    
+    if selected_language == "English":
+        doc_parameter_output = generate_word_document(parameters)
+
+    if selected_language == "French":
+        doc_parameter_output = generate_word_document_FR(parameters_FR)
+
+    zip_file_name = f"PiN_by_indicator_Documents_{country_label}_{datetime.now().strftime('%Y%m%d_%H%M')}.zip"
+    zip_file = create_zip_file_no_ocha(country_label, pin_percentage_output, indicator_output,  doc_parameter_output)
+
+    
+
+    # Create a single download button for the ZIP file
+    if st.download_button(
+        label=translations["download_all"],
+        data=zip_file,
+        file_name=zip_file_name,
+        mime="application/zip"
+    ):
+        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+
+
+        try:
+            repo_name = "martivit/pin-calculation-app"
+            branch_name = "develop_2025"
+
+            # File paths in the repository
+            file_path_in_repo_excel = f"platform_PiN_output/{country}/PiN_by_indicator_results_{country}_{timestamp}.xlsx"
+            file_path_in_repo_excel2 = f"platform_PiN_output/{country}/PiN_percentage_{country}_{timestamp}.xlsx"
+
+            github_token = st.secrets["github"]["token"]
+
+            # Initialize success messages for both uploads
+            pr_url_excel = None
+            pr_url_doc = None
+
+            # Try uploading both files
+            try:
+                pr_url_excel = upload_to_github(
+                    file_content=indicator_output.getvalue(),
+                    file_name=file_path_in_repo_excel,
+                    repo_name=repo_name,
+                    branch_name=branch_name,
+                    commit_message=f"Add PiN results no ocha (Excel) for {country_label}",
+                    token=github_token
+                )
+            except Exception :
+                pass
+                #st.error(f"Failed to upload Excel file to GitHub: {e}")
+
+            try:
+                pr_url_doc = upload_to_github(
+                    file_content=pin_percentage_output.getvalue(),
+                    file_name=file_path_in_repo_excel2,
+                    repo_name=repo_name,
+                    branch_name=branch_name,
+                    commit_message=f"Add PiN pergentage for {country_label}",
+                    token=github_token
+                )
+            except Exception :
+                pass
+                #st.error(f"Failed to upload Word document to GitHub: {e}")
+
+            # Display success messages only if files were successfully uploaded
+            if pr_url_excel:
+                st.success(f"Excel file uploaded to GitHub successfully! [View File]({pr_url_excel})")
+            if pr_url_doc:
+                st.success(f"Word document uploaded to GitHub successfully! [View File]({pr_url_doc})")
+
+        except Exception :
+            #st.error(f"Unexpected error during GitHub upload: {e}")
+            pass
+ 
+    st.subheader(translations["hno_guidelines_subheader"])
+    st.markdown(translations["hno_guidelines_message"])
+
