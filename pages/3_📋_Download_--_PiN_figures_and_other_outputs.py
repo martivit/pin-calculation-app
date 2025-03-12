fa3f43513b6330a3f90efa04bd88eb6a9c67ab73
martivit <martina@10096.it>
2025-03-12 10:03:34 +0100

    test

--- pages/3_📋_Download_--_PiN_figures_and_other_outputs.py
+++ pages/3_📋_Download_--_PiN_figures_and_other_outputs.py
@@ -207,7 +207,7 @@ edu_data_severity = add_severity (country, edu_data, household_data, choice_data
 
 ## calculate PiN
 if ocha_data is not None:
-    (severity_admin_status_list, dimension_admin_status_list, severity_female_list, severity_male_list, factor_category,  pin_per_admin_status) = calculatePIN (country, edu_data_severity, household_data, choice_data, survey_data, ocha_data,mismatch_ocha_data,
+    (severity_admin_status_list, dimension_admin_status_list, severity_female_list, severity_male_lists) = calculatePIN (country, edu_data_severity, household_data, choice_data, survey_data, ocha_data,mismatch_ocha_data,
                                                                                     access_var, teacher_disruption_var, idp_disruption_var, armed_disruption_var,natural_hazard_var,
                                                                                     barrier_var, selected_severity_4_barriers, selected_severity_5_barriers,
                                                                                     age_var, gender_var,
