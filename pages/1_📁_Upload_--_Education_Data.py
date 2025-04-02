7b35cab72b022fe56795fb4df7bbe325911a3661
martivit <martina@10096.it>
2025-04-02 15:43:28 +0200

    Update 1_📁_Upload_--_Education_Data.py

--- pages/1_📁_Upload_--_Education_Data.py
+++ pages/1_📁_Upload_--_Education_Data.py
@@ -271,7 +271,6 @@ else:
                     st.dataframe(df_copy)
                     # Check the first three columns of the first row
                     first_row_check = df_copy.iloc[0, :3].astype(str).str.strip().replace("", pd.NA).notna().sum()
-                    st.dataframe(first_row_check)
 
                     if first_row_check == 0:  # Only modify the copy for the check
                         df_copy.iloc[0, :] = 1  # Modify the copy, not the original
