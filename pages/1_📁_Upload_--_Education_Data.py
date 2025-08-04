76cb74f92140f67b8cafad661e5ac92fe0139bf3
martivit <martina@10096.it>
2025-08-04 14:30:14 +0200

    Update 1_📁_Upload_--_Education_Data.py

--- pages/1_📁_Upload_--_Education_Data.py
+++ pages/1_📁_Upload_--_Education_Data.py
@@ -395,9 +395,10 @@ if is_scenario_2:
     st.subheader(translations["title_pin_upload"])
     with st.container(border=True):
         # Example: expect a structured Excel/CSV input with the extrapolation results
-        updated_2025_pin_file = st.file_uploader(translations["istruction_upload_pin"], type=["xlsx", "csv"])
+        uploaded_updated_2025_pin_file = st.file_uploader(translations["istruction_upload_pin"], type=["xlsx", "csv"])
 
-    if updated_2025_pin_file is not None:
+    if uploaded_updated_2025_pin_file is not None:
+        updated_2025_pin_file = pd.read_excel(uploaded_updated_2025_pin_file, engine='openpyxl', sheet_name=None)
         st.session_state['updated_2025_pin_file'] = updated_2025_pin_file
         try:
             df_updated = st.session_state['updated_2025_pin_file']
