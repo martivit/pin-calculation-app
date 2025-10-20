4bbdd8ca89252a193efbe549cc669c57b849af21
martivit <martina@10096.it>
2025-10-20 14:41:18 +0200

    Update 1_📁_Upload_--_Education_Data.py

--- pages/1_📁_Upload_--_Education_Data.py
+++ pages/1_📁_Upload_--_Education_Data.py
@@ -38,9 +38,9 @@ REQUIRED_COLUMNS = {
     'individual age': {'ind_age', 'age', 'edu_age', 'edu_ind_age', 'age'},
     'admin': {'admin1', 'admin2', 'admin3', 'camp', 'state', 'county', 'district'},
     'edu access': {'edu_access', 'enrolled_school', 'e_enfant_scolarise_formel'},
-    'distruption teacher': {'edu_disrupted_teacher', 'teacher', 'e_absence_enseignant'},
+    'distruption teacher': {'edu_disrupted_teacher', 'teacher', 'e_absence_enseignant', 'enseignant'},
     'distruption hazard': {'edu_disrupted_hazards', 'hazard', 'e_alea'},
-    'distruption displaced': {'edu_disrupted_displaced', 'distrupted_idp', 'e_ecole_abris'},
+    'distruption displaced': {'edu_disrupted_displaced', 'distrupted_idp', 'e_ecole_abris', 'displaced'},
     'edu barrier': {'edu_barrier', 'resn_no_access', 'e_raison_pas_educ_formel'},
     'survey start': {'start', 'date', 'start_time', 'Start_datetime', 'survey_start_date', 'today'}
 }
