e61c1b6b480b6c82ec1e1295af0f73c33e925796
martivit <martina@10096.it>
2025-11-21 11:29:07 +0100

    Update snapshot_PiN_FR.py

--- src/snapshot_PiN_FR.py
+++ src/snapshot_PiN_FR.py
@@ -405,6 +405,47 @@ def create_snapshot_PiN_FR(country_label, final_overview_df, final_overview_df_O
     label_admin_severity= 'Sévérité de la zone'
     label_tot_population= 'Population totale'
 
+
+        # Constants
+    int_2 = '2.0'
+    int_3 = '3.0'
+    int_4 = '4.0'
+    int_5 = '5.0'
+    label_perc2 = '% severity levels 1-2'
+    label_perc3 = '% severity level 3'
+    label_perc4 = '% severity level 4'
+    label_perc5 = '% severity level 5'
+    label_tot2 = '# severity levels 1-2'
+    label_tot3 = '# severity level 3'
+    label_tot4 = '# severity level 4'
+    label_tot5 = '# severity level 5'
+    label_perc_tot = '% Tot PiN (severity levels 3-5)'
+    label_tot = '# Tot PiN (severity levels 3-5)'
+    label_admin_severity = 'Area severity'
+    label_tot_population = 'TotN'
+
+    int_acc = 'access'
+    int_agg= 'aggravating circumstances'
+    int_lc = 'learning condition'
+    int_penv = 'protected environment'
+    int_out = 'Not in need'
+    label_perc_acc = '% Access'
+    label_perc_agg= '% Aggravating circumstances'
+    label_perc_lc = '% Learning conditions'
+    label_perc_penv = '% Protected environment'
+    label_perc_out = '% Not in need'
+    label_tot_acc = '# Access'
+    label_tot_agg= '# Aggravating circumstances'
+    label_tot_lc = '# Learning conditions'
+    label_tot_penv = '# Protected environment'
+    label_tot_out = '# Not in need'
+    label_dimension_perc_tot = '% Tot in PiN Dimensions'
+    label_dimension_tot = '# Tot in PiN Dimensions'
+    label_dimension_tot_population = 'TotN'
+
+
+
+
     color_mapping = {
         label_perc2: colors["light_beige"],
         label_tot2: colors["light_beige"],
